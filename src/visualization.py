48d16d94a693bb6b399ecba9950eccceafe087d6
Szakacs Daniel <daniel.sokach@gmail.com>
2025-03-03 20:28:17 +0100

    visualize histplot function created

--- src/visualization.py
+++ src/visualization.py
@@ -27,3 +27,28 @@ def show_boxplots(columns: list, df: pd.DataFrame) -> None:
 
   plt.tight_layout()
   plt.show()
+
+
+def show_histplot(columns: list, df: pd.DataFrame) -> None:
+  """
+  Creates boxplots for float-type features in the given DataFrame.
+
+    Args:
+        columns (list): List of feature names that are of float type.
+        df (pd.DataFrame): The DataFrame containing the features.
+
+    Returns:
+        None: Displays the boxplots in a grid with up to 3 columns per row.
+  """
+  row = max(1, np.ceil(len(columns) / 3).astype(int))
+  fig, axes = plt.subplots(row, 3, figsize=(15, 5 * row))
+
+  if row == 1:
+      axes = np.array(axes).reshape(1, -1) 
+
+  for index, name in enumerate(columns): 
+      r, c = divmod(index, 3)
+      sns.histplot(x=name, data=df, ax=axes[r, c])
+
+  plt.tight_layout()
+  plt.show()
